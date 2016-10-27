8ff72631a5bfa51a5e6778a557148a91574c1453
Shi Ping <limshiping93@hotmail.com>
2016-10-27 21:30:06 +0800

    Mail: made message optional

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -3,7 +3,6 @@
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
    empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
    echo "No arguments Provided!";
